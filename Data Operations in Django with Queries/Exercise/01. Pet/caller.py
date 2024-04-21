3c025c8f91af94a62a5423f9137dea69fa3ca085
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-04-21 11:00:05 +0300

    Add files via upload

---
+++ Data Operations in Django with Queries/Exercise/01. Pet/caller.py
@@ -0,0 +1,14 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+from main_app.models import Pet
+
+# Create queries within functions
+def create_pet(name: str, species: str): ## first task
+  Pet.objects.create(name=name, species=species)
+  return f"{name} is a very cute {species}!"
