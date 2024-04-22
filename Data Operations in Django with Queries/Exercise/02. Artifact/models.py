fb425f5b412488d7a51e0afbe0e42f97b4364f5c
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-04-22 19:41:46 +0300

    Add files via upload

---
+++ Data Operations in Django with Queries/Exercise/02. Artifact/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Pet(models.Model): ## first task
+  name = models.CharField(max_length=40)
+  species = models.CharField(max_length=40)
+
+
+class Artifact(models.Model): ## second task
+  name = models.CharField(max_length=70)
+  origin = models.CharField(max_length=70)
+  age = models.PositiveIntegerField()
+  description = models.TextField()
+  is_magical = models.BooleanField(default=False)
