5c1324efcf96c8377354c4722bdcf01c67b3d1f7
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-04-24 14:37:39 +0300

    Add files via upload

---
+++ Data Operations in Django with Queries/Exercise/04. Car/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Pet(models.Model): ## first task
+  name = models.CharField(max_length=40)
+  species = models.CharField(max_length=40)
+
+
+class Artifact(models.Model): ## second task
+  name = models.CharField(max_length=70)
+  origin = models.CharField(max_length=70)
+  age = models.PositiveIntegerField()
+  description = models.TextField()
+  is_magical = models.BooleanField(default=False)
+
+
+class Location(models.Model): # third task
+  name = models.CharField(max_length=100)
+  region = models.CharField(max_length=50)
+  population = models.PositiveIntegerField()
+  description = models.TextField()
+  is_capital = models.BooleanField(default=False)
+
+
+
+class Car(models.Model): # fourth task
+  model = models.CharField(max_length=40)
+  year = models.PositiveIntegerField()
+  color = models.CharField(max_length=40)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  price_with_discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
