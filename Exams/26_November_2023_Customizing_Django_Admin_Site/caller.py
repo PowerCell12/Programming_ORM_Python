e17582fa62d24a1d0560ff1c3238810d3a3a83ab
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-06-01 13:37:00 +0300

    Delete Exams/26_November_2023_Customizing_Django_Admin_Site/caller.py

--- Exams/26_November_2023_Customizing_Django_Admin_Site/caller.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-
-# Set up Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
-django.setup()
-
-# Import your models here
-# Create and run your queries within functions
