8fdd2c0701868eb9dc6d701b1874062338eb38c5
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-06-13 13:28:21 +0300

    Add files via upload

---
+++ Exams/Exam_Prep_I_Django_Queries_II/models.py
@@ -0,0 +1,48 @@
+from django.core import validators
+from django.db import models
+from django.db.models import Count
+
+
+# Create your models here.
+
+
+class DirectorManager(models.Manager):
+    def get_directors_by_movies_count(self):
+        return self.annotate(count_movies=Count('movies__id')).order_by('-count_movies', 'full_name')
+
+
+class Director(models.Model):
+    full_name = models.CharField(max_length=120, validators=[validators.MinLengthValidator(2)])
+    birth_date = models.DateField(default='1900-01-01')
+    nationality = models.CharField(max_length=50, default='Unknown')
+    years_of_experience = models.SmallIntegerField(default=0, validators=[validators.MinValueValidator(0)])
+    objects = DirectorManager()
+
+
+class Actor(models.Model):
+    full_name = models.CharField(max_length=120, validators=[validators.MinLengthValidator(2)])
+    birth_date = models.DateField(default='1900-01-01')
+    nationality = models.CharField(max_length=50, default='Unknown')
+    is_awarded = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now=True)
+
+CHOICES  = [
+    ('Action', 'Action'),
+    ('Comedy', 'Comedy'),
+    ('Drama', 'Drama'),
+    ('Other', 'Other')
+]
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=150, validators=[validators.MinLengthValidator(5)])
+    release_date = models.DateField()
+    storyline = models.TextField(blank=True, null=True)
+    genre = models.CharField(max_length=6, default='Other', choices=CHOICES)
+    rating = models.DecimalField(default=0.0, max_digits=3, decimal_places=1, validators=[validators.MaxValueValidator(10), validators.MinValueValidator(0)]) ## from here
+    is_classic = models.BooleanField(default=False)
+    is_awarded = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now=True)
+    director = models.ForeignKey(to=Director, on_delete=models.CASCADE, related_name='movies')
+    starring_actor = models.ForeignKey(to=Actor, blank=True, null=True, on_delete=models.SET_NULL, related_name='movies')
+    actors= models.ManyToManyField(to=Actor)
